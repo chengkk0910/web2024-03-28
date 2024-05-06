ce2181b33481fcbeb8a59071ea527d1b06ce5ff8
kai <cgk0910@gmail.com>
2024-05-06 16:05:59 +0800

    php

--- s20240506/test.php
+++ s20240506/test.php
@@ -1,23 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+$indexArr = [1, 2, 3, 4, 5];
 
-<body>
-    <?php for ($i = 1; $i <= 6; $i += 2) : ?>
-        <!-- if -->
-        <div class="item">
+$assArr = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
 
-        </div>
-        <!-- else -->
-        <div class="item item-hide">
-            6
-        </div>
-    <?php endfor; ?>
-</body>
+$assArr = [
+    'day1' => [
+        'date' => "5/6",
+        'rest' => true,
+        'desc' => "test 12345",
+        'skills' => [
+            'skill1' => 'PHP',
+            'skill2' => 'HTML'
+        ]
+    ],
+    'day2' => 'bob',
+    'day3' => 'cat'
+];
 
-</html>
+dd($indexArr);
+dd($assArr);
+dd($assArr['day1']['desc']);
+dd($assArr['day1']['skills']['skill1']);
+dd($assArr['day2']);
+dd($assArr['day3']);
+
+// 5 , monthArr = [1,2,3,4,5..., 31]
+// restArr = [5/2,5/8]
+
+// foreach monthArr
+   // if monthArr Value , 存在restArr ?
