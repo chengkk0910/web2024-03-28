6168e761b36955764d7e7484b1199813db116128
kai <cgk0910@gmail.com>
2024-05-06 11:24:38 +0800

    flexbox

---
+++ s20240506/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php for ($i = 1; $i <= 6; $i += 2) : ?>
+        <!-- if -->
+        <div class="item">
+
+        </div>
+        <!-- else -->
+        <div class="item item-hide">
+            6
+        </div>
+    <?php endfor; ?>
+</body>
+
+</html>
