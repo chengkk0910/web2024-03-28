7651d48f5cf539470e82c45b5659eb2161333227
kai <cgk0910@gmail.com>
2024-05-06 16:26:21 +0800

    php to html

--- s20240506/test.php
+++ s20240506/test.php
@@ -8,7 +8,7 @@ function dd($data)
 
 $indexArr = [1, 2, 3, 4, 5];
 
-$assArr = [
+$studentArr = [
     's1' => 'amy',
     's2' => 'bob',
     's3' => 'cat'
@@ -28,15 +28,88 @@ $assArr = [
     'day3' => 'cat'
 ];
 
-dd($indexArr);
-dd($assArr);
-dd($assArr['day1']['desc']);
-dd($assArr['day1']['skills']['skill1']);
-dd($assArr['day2']);
-dd($assArr['day3']);
+$superArr = [
+    's1' => [
+        'name' => 'amy',
+        'mobile' => '0911',
+    ],
+    's2' => [
+        'name' => 'bob',
+        'mobile' => '0922',
+    ],
+    's3' => [
+        'name' => 'cat',
+        'mobile' => '0933',
+    ]
+];
+
+// dd($studentArr);
+dd($superArr);
+// dd($assArr);
+// dd($assArr['day1']['desc']);
+// dd($assArr['day1']['skills']['skill1']);
+// dd($assArr['day2']);
+// dd($assArr['day3']);
 
 // 5 , monthArr = [1,2,3,4,5..., 31]
 // restArr = [5/2,5/8]
 
 // foreach monthArr
-   // if monthArr Value , 存在restArr ?
+// if monthArr Value , 存在restArr ?
+?>
+
+<!-- html -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>HTML Table</h2>
+
+    <table>
+        <tr>
+            <th>sn.</th>
+            <th>name</th>
+            <th>mobile</th>
+        </tr>
+        <?php foreach ($superArr as $key => $value) : ?>
+            <tr>
+                <td>
+                    <?= $key; ?>
+                </td>
+                <td>
+                    <?= $value['name']; ?>
+                </td>
+                <td>
+                    <?= $value['mobile']; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
+
+</body>
+
+</html>
