a24b94045d04f7eaa0561c5ff5f886728b54c3a5
kai <cgk0910@gmail.com>
2024-06-05 16:17:48 +0800

    class student

---
+++ s20240605/s20240605_02.php
@@ -0,0 +1,40 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "<//pre>";
+}
+
+// 學生資料表
+// 1.amy 0911-111-111
+// 2.bob 0922-222-222
+// 3.cat 0933-333-333
+
+
+class Student
+{
+    public $name;
+    public $mobile;
+    public $num;
+
+    public function intro()
+    {
+        $result = "Hi , I'm $this->num $this->name - $this->mobile";
+        echo "$result <br>";
+    }
+}
+
+$s1 = new Student();
+$s1->name = 'amy';
+$s1->mobile = '0911-111-111';
+$s1->num = 1;
+dd($s1);
+$s1->intro();
+
+$s2 = new Student();
+$s2->name = 'bob';
+$s2->mobile = '0922-222-222';
+$s2->num = 2;
+dd($s2);
+$s2->intro();
