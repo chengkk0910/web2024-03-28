ac68d999c6039523670c0dc7e20d5317eb28c2e5
kai <cgk0910@gmail.com>
2024-06-07 10:36:54 +0800

    form

---
+++ s20240607/test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `test`.`students` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR NOT NULL,
+    `mobile` VARCHAR NOT NULL,
+    `address` VARCHAR NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
